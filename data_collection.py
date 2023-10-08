e72261b28547f0e75bd8dd9eca768cccddbaacd1
Ypaspistis Giannis <giannisypaspistis@gmail.com>
2023-10-08 22:33:00 +0300

    managed to parse the jobs data into a csv

--- data_collection.py
+++ data_collection.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 path = "F:/New_Python_staff/ds_salaries/chromedriver.exe"
 
-df = gs.get_jobs('data scientist',1000, False, path, 15)
+df = gs.get_jobs('data scientist',15, False, path, 15)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
+
